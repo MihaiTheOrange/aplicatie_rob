f8de30482184b747d8db6669164b950b16887823
MihaiTheOrange <mohaimihai22@gmail.com>
2024-04-12 18:54:16 +0300

    Universal cleanup+ Live database updates

--- apptest/api.py
+++ apptest/api.py
@@ -1,19 +1,28 @@
 import requests
 
-BASE_URL='http://127.0.0.1:8000'
-ENDPOINT='/sen1'
-def get_data_from_fastapi():
+
+def get_data_from_fastapi(endpoint, app):
     try:
-        response = requests.get(BASE_URL+ENDPOINT)
+        #app=App.get_running_app()
+        BASE_URL = app.base_url
+        response = requests.get(BASE_URL+endpoint)
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             data = response.json()
             # Process the data received from FastAPI server
-            print("Data received:", data)
+            return data
         else:
-            print("Error:", response.status_code)
+            return 'error'
     except requests.exceptions.RequestException as e:
-        print("Error:", e)
+        return 'error'
 
-# Call the function to get data from FastAPI server
-get_data_from_fastapi()
+
+def post_to_api(endpoint, payload, app):
+    #app = App.get_running_app()
+    BASE_URL = app.base_url
+    response = requests.post(BASE_URL+endpoint, json=payload)
+    if response.status_code == 200:
+        print('POST request was successful!')
+        print('Response:', response.text)
+    else:
+        print('POST request failed:', response.status_code)
--- backendSEBASTIEN/main.py
+++ backendSEBASTIEN/main.py
@@ -10,9 +10,7 @@ models.Base.metadata.create_all(bind=engine)
 
 
 class Senzor1(BaseModel):
-    id: int
     inp: Optional[int] = None
-    data: datetime
 
 
 def deschide():
@@ -32,17 +30,15 @@ sen1_router = APIRouter(
 
 
 @sen1_router.get('', operation_id="sen1data")
-def get_data_query(limit: int = 10, db: Session = Depends(deschide)):
-    data = list(db.query(models.Senzor1).all())
-    return data[:limit]
+def get_data_query(db: Session = Depends(deschide)):
+    data = db.query(models.Senzor1).all()
+    return data
 
 
 @sen1_router.post('', operation_id="sen1post")
 def create_log(log: Senzor1, db: Session = Depends(deschide)):
     log_model = models.Senzor1()
-    log_model.id = log.id
     log_model.inp = log.inp
-    log_model.data_in = log.data
     db.add(log_model)
     db.commit()
     return {'message': f'succes added input:{id}'}
@@ -50,55 +46,6 @@ def create_log(log: Senzor1, db: Session = Depends(deschide)):
 
 app.include_router(sen1_router)
 
-sen2_router = APIRouter(
-    prefix='/sen2',
-    tags=['senzor_2']
-)
-
-
-@sen2_router.get('', operation_id="sen2data")
-def sen2data(limit: int = 10, db: Session = Depends(deschide)):
-    data = list(db.query(models.Senzor2).all())
-    return data[:limit]
-
-
-@sen2_router.post('', operation_id="sen2post")
-def sen2post(log: Senzor1, db: Session = Depends(deschide)):
-    log_model = models.Senzor2()
-    log_model.id = log.id
-    log_model.inp = log.inp
-    log_model.data_in = log.data
-    db.add(log_model)
-    db.commit()
-    return {'message': f'succes added input:{id}'}
-
-
-app.include_router(sen2_router)
-
-sen3_router = APIRouter(
-    prefix='/sen3',
-    tags=['senzor_3']
-)
-
-
-@sen3_router.get('', operation_id="sen3data")
-def sen3data(limit: int = 10, db: Session = Depends(deschide)):
-    data = list(db.query(models.Senzor3).all())
-    return data[:limit]
-
-
-@sen3_router.post('', operation_id="sen3post")
-def sen3post(log: Senzor1, db: Session = Depends(deschide)):
-    log_model = models.Senzor3()
-    log_model.id = log.id
-    log_model.inp = log.inp
-    log_model.data_in = log.data
-    db.add(log_model)
-    db.commit()
-    return {'message': f'succes added input:{id}'}
-
-
-app.include_router(sen3_router)
 
 comenzi_router = APIRouter(
     prefix='/comenzi',
